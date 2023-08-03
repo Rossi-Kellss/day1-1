014bbb504cd0572c9833f92ab862443e5fd69bc7
Rossi-Kellss <137304227+Rossi-Kellss@users.noreply.github.com>
2023-08-03 10:22:39 +0000

    if else

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const 
+let a=prompt("Enter a number");
+a=Number.parseInt(a);
+console.log((a>10)?"greater than 10":"lesser than 10");
+// console.log(typeof a)
+
+
+const expr='banana'
+
+switch(expr){
+    case "banana":
+    console.log("It is bananna");
+    break;
+    case "mango":
+    console.log("it is mango");
+    break;
+
+  
+}
