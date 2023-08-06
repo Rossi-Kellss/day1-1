b73a153f76967e13f4b66ddfa36c13d500c7048f
Rossi-Kellss <137304227+Rossi-Kellss@users.noreply.github.com>
2023-08-06 11:44:42 +0000

    string

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-let a=prompt("Enter a number");
-a=Number.parseInt(a);
-console.log((a>10)?"greater than 10":"lesser than 10");
-// console.log(typeof a)
+// // Exercise1
+// let age=prompt("Enter your age");
+// age=Number.parseInt(age);
 
+// if(age>10&&age<20){
+//   console.log("Age is as expected");
+// }
+// else{
+//   console.log("Age is not as expected");
+// }
 
-const expr='banana'
-
-switch(expr){
-    case "banana":
-    console.log("It is bananna");
-    break;
-    case "mango":
-    console.log("it is mango");
-    break;
-
+// exercise on looops
+const data={
+  rossi:20,
+  kells:14,
+  james:34,
   
 }
+console.log(Object.keys(data).length);
+console.log(data[Object.keys(data)])
+// console.log(Object.keys(data))
+
+for(i=0;i<Object.keys(data).length;i++){
+  console.log("The marks of "+Object.keys(data)[i]+ 'are' +data[Object.keys(data)[i]]);
+  
+}
+
+for(key in data){
+  console.log("the marks of "+key+ "are" +data[key]);
+}
+
+
+const str='bana\a'
+console.log(str.length);
+
